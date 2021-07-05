f0562371717caa9f9a7539b2c32a2ffbce125af1
Ahmed Sami <ahmed.sami.elsherif@gmail.com>
2021-07-05 03:31:31 +0200

    :bug: fix routes order bug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,14 @@ import { HomePage, LandingPage } from "../pages"
 
 const routes = [
     {
-        key: 'landing',
-        path:'/',
-        component: LandingPage
-    },
-    {
         key: 'home',
         path:'/home',
         component: HomePage
+    },
+    {
+        key: 'landing',
+        path:'/',
+        component: LandingPage
     },
 ]
 
